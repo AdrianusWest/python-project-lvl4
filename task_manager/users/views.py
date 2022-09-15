7f14bbaa2f3662a6dbce1649942e64caff9eafb4
Adrianus West <adrianus_west@protonmail.com>
2022-09-15 16:37:12 +0300

    Update: setup django to 4.0.6

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -68,7 +68,7 @@ class SignIn(SuccessMessageMixin, LoginView):
     template_name = 'users/sign_in.html'
     form_class = SignInForm
     next_page = reverse_lazy('main')
-    success_message = gettext_lazy('Вы залогинены')
+    success_message = gettext_lazy('Вы в системе')
 
 
 class SignOut(LogoutView, SuccessMessageMixin):
